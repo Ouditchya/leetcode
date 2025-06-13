2441ff003badd528104687280113d468380ce6d2
Ouditchya Sinha <osinha6792@gmail.com>
2025-06-13 09:24:30 +0530

    Add files via upload

---
+++ python/3423. Maximum Difference Between Adjacent Elements in a Circular Array.py
@@ -0,0 +1,5 @@
+class Solution:
+    def maxAdjacentDistance(self, nums: List[int]) -> int:
+        maxDiff = abs(nums[0]-nums[-1])
+        for i in range(1, len(nums)): maxDiff = max(maxDiff, abs(nums[i]-nums[i-1]))
+        return maxDiff
